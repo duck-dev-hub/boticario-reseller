0eecc90e3a3af5f9ba9467e84cccabb5f9725498
Rodrigo Reis <roreis99@gmail.com>
2019-11-21 17:04:32 -0300

    feat: finish styled registration page

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Input from './styles';
 
-const InputField = ({ type, required }) => {
-  return <Input type={type} required={required} />;
+const InputField = ({ type, required, placeholder }) => {
+  return (
+    <Input
+      type={type}
+      required={required}
+      placeholder={placeholder}
+    />
+  );
 };
 
 export default InputField;
