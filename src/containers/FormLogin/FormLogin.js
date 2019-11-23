daba4fd607a988408e155fe5bd30b3f610134726
Rodrigo Reis <roreis99@gmail.com>
2019-11-23 15:16:37 -0300

    merge fix

--- src/containers/FormLogin/FormLogin.js
+++ src/containers/FormLogin/FormLogin.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 import FormModal from '../../components/FormModal/FormModal';
 import InputField from '../../components/InputField';
@@ -7,12 +8,45 @@ import LinkForm from '../../components/LinkForm';
 import MainButton from '../../components/MainButton';
 
 const FormLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const getUser = async () => {
+    const endpoint = 'http://localhost:8000/auth/login';
+    try {
+      const response = await Axios.post(endpoint, {
+        email,
+        password,
+      });
+      const { data } = response;
+      console.log('foo', data);
+      return data;
+    } catch (error) {
+      console.log('bar', error);
+      return false;
+    }
+  };
+
+  const handleLogin = ev => {
+    ev.preventDefault();
+    console.log('eae');
+    getUser();
+  };
+
   return (
-    <FormModal title="Login">
+    <FormModal onSubmit={ev => handleLogin(ev)} title="Login">
       <InputName content="Email" />
-      <InputField type="email" required />
+      <InputField
+        type="email"
+        required="required"
+        onChange={ev => setEmail(ev.target.value)}
+      />
       <InputName content="Senha" />
-      <InputField type="password" />
+      <InputField
+        type="password"
+        required="required"
+        onChange={ev => setPassword(ev.target.value)}
+      />
       <LinkForm to="/" content="Esqueceu sua senha?" right="true" />
       <MainButton Primary type="submit" content="Entrar" />
       <LinkForm to="/cadastro" content="Cadastro" />
