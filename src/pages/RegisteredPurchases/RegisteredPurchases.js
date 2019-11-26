0ca9744139c771b3051f97e87e4b7a90cc2fdb50
Rodrigo Reis <roreis99@gmail.com>
2019-11-25 21:12:33 -0300

    gitflow-feature-stash: registered-purchases

---
+++ src/pages/RegisteredPurchases/RegisteredPurchases.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ShippingsCards from '../../containers/ShippingsCards';
+
+const RegisteredPurchases = () => {
+  return <ShippingsCards />;
+};
+
+export default RegisteredPurchases;
